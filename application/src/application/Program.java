1be73381c118f406f54b9e521d930eb70a85b70f
ellisroberta <ellisroberta@hotmail.com>
2024-11-21 14:35:12 -0300

    realizacao do exercicio demo Generic

--- application/src/application/Program.java
+++ application/src/application/Program.java
@@ -9,17 +9,17 @@ public class Program {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        PrintService ps = new PrintService();
+        PrintService <String> ps = new PrintService<>();
 
         System.out.print("How many values? ");
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            Integer value = sc.nextInt();
+            String value = sc.next();
             ps.addValue(value);
         }
         ps.print();
-        Integer x = (Integer) ps.first();
+        String x = ps.first();
         System.out.println("First: " + x);
 
         sc.close();
