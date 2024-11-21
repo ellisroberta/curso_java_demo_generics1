278d3f91faab2f9c27a1b421eb1f31642d092557
ellisroberta <ellisroberta@hotmail.com>
2024-11-21 14:03:52 -0300

    edicao do README e realizacao do exercicio demo Object

--- README.md
+++ README.md
@@ -4,6 +4,25 @@ inteiros. Ao final, imprima esses números de forma organizada conforme
 exemplo. Em seguida, informar qual foi o primeiro valor informado.
 
 Criar um serviço de impressão:
+
+![](Integer.png)
+```
+How many values? 3 
+10
+8
+23
+[10, 8, 23]
+First: 10
+```
+# Problema motivador 2 (type safety & performance)
+Deseja-se fazer um programa que leia uma quantidade N, e depois N nomes de
+pessoas. Ao final, imprima esses números de forma organizada conforme
+exemplo. Em seguida, informar qual foi o primeiro valor informado.
+
+Criar um serviço de impressão:
+
+![](Object.png)
+
 ```
 How many values? 3 
 10
--- application/src/application/Program.java
+++ application/src/application/Program.java
@@ -5,7 +5,9 @@ import model.services.PrintService;
 import java.util.Scanner;
 
 public class Program {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         PrintService ps = new PrintService();
 
@@ -13,11 +15,12 @@ public class Program {
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            int value = sc.nextInt();
+            Integer value = sc.nextInt();
             ps.addValue(value);
         }
         ps.print();
-        System.out.println("First: " + ps.first());
+        Integer x = (Integer) ps.first();
+        System.out.println("First: " + x);
 
         sc.close();
     }
